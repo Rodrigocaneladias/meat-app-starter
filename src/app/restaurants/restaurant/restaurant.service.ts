cff0972dcfe79b5fd8e31c74b56b9953d6556575
DIR\rodrigo.canela.dias <rodrigo.canela.dias@accenture.com>
2018-09-27 15:32:16 -0300

    add dmenu

--- src/app/restaurants/restaurant/restaurant.service.ts
+++ src/app/restaurants/restaurant/restaurant.service.ts
@@ -6,6 +6,7 @@ import "rxjs/add/operator/map"
 import "rxjs/add/operator/catch"
 
 import { Restaurant } from "./restaurant.model";
+import { MenuItem } from './../../restaurant-detail/menu-item/menu-item.model';
 import { MEAT_API } from './../../api.app';
 import { ErrorHandler } from './../../app.error-handler';
 
@@ -20,6 +21,18 @@ export class RestaurantService {
 
     restaurantById(id: string): Observable<Restaurant>{
         return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
