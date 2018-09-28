8e1b2ef0d1b615702e89d4dd17ed3c1c3a1ef7cd
unknown <rodrigocaneladias@ymail.com>
2018-09-27 21:16:03 -0300

    finalizando carrinho

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -4,17 +4,15 @@ import { ShoppingCartService } from './shopping-cart.service';
 import { CartItem } from './cart-item.model';
 
 @Component({
-  selector: 'mt-shopping-cart',
-  templateUrl: './shopping-cart.component.html'
+  selector: "mt-shopping-cart",
+  templateUrl: "./shopping-cart.component.html"
 })
 export class ShoppingCartComponent implements OnInit {
+  constructor(private shoppingCartService: ShoppingCartService) {}
 
-  constructor(private shoppingCartService: ShoppingCartService) { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  itens(): CartItem[]{
+  itens(): CartItem[] {
     return this.shoppingCartService.itens;
   }
 
@@ -22,4 +20,15 @@ export class ShoppingCartComponent implements OnInit {
     return this.shoppingCartService.total();
   }
 
+  clear(): void {
+    this.shoppingCartService.clear();
+  }
+
+  removeItem(item: any): void {
+    this.shoppingCartService.removeItem(item);
+  }
+
+  addItem(item: any): void {
+    this.shoppingCartService.addItem(item);
+  }
 }
