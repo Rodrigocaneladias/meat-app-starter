1653748fef2282799c851cdcaa64d348da7f77ed
DIR\rodrigo.canela.dias <rodrigo.canela.dias@accenture.com>
2018-10-02 17:26:46 -0300

    Radio buttons Controllvalueaccessor

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from '../common/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  paymentOptions: RadioOption[] =[
+    {value:"MONEY" , label:"Dinheiro" },
+    {value:"DEB" , label:"Cartão de Débito" },
+    {value:"REF" , label:"Vale Refeição" }
+  ] 
 
   constructor() { }
 
